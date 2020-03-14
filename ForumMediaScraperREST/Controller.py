4b4c3fc5f977f9a1cb7da688193172385e8fa8d4
jesse <jessevanderwolf@gmail.com>
2020-03-14 22:00:04 +0100

    Removed submodule

---
+++ ForumMediaScraperREST/Controller.py
@@ -0,0 +1,71 @@
+import os
+import pytz
+import json
+from flask import Flask
+from gridfs import GridFS
+from bson import ObjectId
+from datetime import datetime
+from pymongo import MongoClient
+from ForumMediaScraper.Scraper import ScraperConfig
+
+_WEBSERVICE_SETTINGS = {
+    'SCRAPER_SHUTDOWN_BUFFER': 20,
+    'SCRAPER_RUN_INTERVAL': 120
+}
+
+
+class FlaskController:
+    """
+    The flask controller has 3 tasks:
+    1. Manage the webservice its internal state using the config.json file
+    2. Manage external connections the webservice uses
+    3. Manage the internal scheduler for running the scraper
+    """
+
+    def __init__(self, app: Flask, config: ScraperConfig):
+        self.logger = app.logger
+        self.timezone = pytz.timezone('Europe/Berlin')
+
+        self.mongo_client = MongoClient(**config.get_mongo_config())
+        self.mongo_database = self.mongo_client['ForumMediaData']
+        self.mongo_gridfs = GridFS(database=self.mongo_database)
+
+        self._app = app
+        self._scraper_config = config.update(_WEBSERVICE_SETTINGS)
+        self._config_file = 'config.json'
+        self._datetime_format = "%Y-%m-%d %H:%M:%S"
+        self._service_config = self.load_config()
+
+    def load_config(self):
+        config = {}
+        mode = 'r'
+        if not os.path.isfile(self._config_file):
+            mode = 'w+'
+        with open(self._config_file, mode=mode) as f:
+            if mode == 'w+':
+                for key, value in self._scraper_config:
+                    if os.getenv(key):
+                        config[key] = os.getenv(key)
+                    else:
+                        config[key] = value
+                f.write(json.dumps(config))
+            f.seek(0)
+            return json.loads(f.read())
+
+    def put_config(self, new: dict):
+        config = self.load_config()
+        config.update(new)
+        with open(self._config_file, mode='w+') as f:
+            f.write(json.dumps(config))
+            return config
+
+    def convert_objects(self, doc: dict):
+        for key, value in doc.items():
+            if isinstance(value, datetime):
+                doc[key] = value.strftime(self._datetime_format)
+            elif isinstance(value, ObjectId):
+                doc[key] = str(value)
+            elif isinstance(value, list):
+                for document in value:
+                    self.convert_objects(document)
+        return doc
--- ForumMediaScraperREST/__init__.py
+++ ForumMediaScraperREST/__init__.py
@@ -1,23 +1,28 @@
 import json
 import base64
+from pymongo.errors import AutoReconnect
+from ForumMediaScraper.Scraper import ScraperConfig
+from ForumMediaScraperREST.Controller import FlaskController
 from flask import (
     Flask,
     request,
     Response
 )
-from pymongo.errors import AutoReconnect
-from .FlaskController import (
-    FlaskController,
-    MediaScraperStillRunningException,
-    InvalidControllerException,
-    InvalidConfigException
-)
+
+config = ScraperConfig({
+    'WEBDRIVER_EXECUTABLE_PATH': './drivers/geckodriver-win.exe',
+    'MONGO_INITDB_ROOT_USERNAME': 'admin',
+    'MONGO_INITDB_ROOT_PASSWORD': 'password123',
+    'SCRAPER_CREATE_LOGFILE': True,
+    'SCRAPER_HEADLESS_MODE': False,
+    'SCRAPER_MAX_SCROLL_SECONDS': 40,
+    'WEBDRIVER_BROWSER_EXECUTABLE_PATH': 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+})
 
 app = Flask(__name__)
-flask_controller = FlaskController(app=app)
+controller = FlaskController(app, config)
 
 
-# query route that provides an interface to the MongoDB database
 @app.route('/query', methods=['GET'])
 def query():
     status = 200
@@ -25,7 +30,6 @@ def query():
     limit = request.args.get('limit') if request.args.get('limit') else 5
     offset = request.args.get('offset') if request.args.get('offset') else 0
     try:
-        # create mongodb aggregation pipeline
         pipeline = [
             {"$sort": {"StartScrapeTime": -1}},
             {"$skip": int(offset)},
@@ -39,13 +43,12 @@ def query():
                 }
             }
         ]
-        # build response json body
-        for run in flask_controller.mongo_database['Runs'].aggregate(pipeline=pipeline):
+        cursor = controller.mongo_database['Runs'].aggregate(pipeline)
+        for run in cursor:
             for post in run['Posts']:
-                gridfs_file = flask_controller.mongo_gridfs.get(post['MediaId'])
-                post['MediaData'] = base64.b64encode(gridfs_file.read(size=-1)).decode('utf-8')
-
-            run = flask_controller.object_to_string(doc=run)
+                file = controller.mongo_gridfs.get(post['MediaId'])
+                post['file'] = base64.b64encode(file.read(size=-1)).decode('utf-8')
+            run = controller.convert_objects(run)
             body['documents'].append(run)
     except AutoReconnect as MongoError:
         app.logger.warning('Error reconnecting to the mongo database: {err}'.format(err=MongoError))
@@ -54,46 +57,22 @@ def query():
     return Response(response=json.dumps(body), status=status, content_type='application/json')
 
 
-# config route that provides an interface to the internal configuration of the flask webservice
 @app.route('/config', methods=['GET', 'PUT'])
 def config():
     status = 200
     response_body = {'success': True}
     try:
         if request.method == 'GET':
-            response_body['config'] = flask_controller.get_config()
-
-        if request.method == 'PUT':
-            request_body = request.get_json()
-            conf = flask_controller.update_internal_conf(config=request_body)
-            response_body['config'] = conf
-
-    except json.decoder.JSONDecodeError as decodeError:
-        response_body.update({
-            'success': False,
-            'error': {
-                'type': 'json.decoder.JSONDecodeError',
-                'message': decodeError
-            }
-        })
-        status = 400
-    except MediaScraperStillRunningException as runningJob:
-        response_body.update({
-            'success': False,
-            'error': {
-                'type': 'MediaScraperStilRunning',
-                'message': runningJob.args[0]
-            }
-        })
-        status = 409
-    except (InvalidControllerException, InvalidConfigException):
-        response_body.update({
-            'success': False,
-            'error': {
-                'type': 'InvalidControllerException',
-                'message': 'Error validating the controller config, check webserver logs for further information'
-            }
-        })
+            response_body['config'] = controller.load_config()
+        elif request.method == 'PUT':
+            request_body = request.get_json(silent=True)
+            if request_body is None:
+                raise Exception('Failed to parse json')
+            new_config = controller.put_config(request_body)
+            response_body['config'] = new_config
+    except Exception as err:
+        app.logger.warning('Request failed with reason: %s' % str(err))
+        status = 500
     return Response(response=json.dumps(response_body), status=status, content_type='application/json')
 
 
